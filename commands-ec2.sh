0fa640d048073031216b5e112e79cced8361ec7a
Borel KOUMO <borelkoumo@gmail.com>
2022-05-29 10:51:35 +0200

    Fixing erros messages

--- commands-ec2.sh
+++ commands-ec2.sh
@@ -4,7 +4,7 @@ git add .; git commit -m "Refactoring Docker image" ; git push
 # Pull and build Docker image
 sudo -i
 cd /home/ec2-user/march1st-backend
-git pull && node ace build --production --ignore-ts-errors && docker build -t "march1st-backend" . && docker run -itd --name march1st-backend -p8080:8080 march1st-backend && docker logs --follow --tail 100 march1st-backend
+git pull && node ace build --production --ignore-ts-errors && docker rm -f march1st-backend && docker rmi march1st-backend && docker build -t "march1st-backend" . && docker run -itd --name march1st-backend -p8080:8080 march1st-backend && docker logs --follow --tail 100 march1st-backend
 
 # Run container
 docker run -itd --name march1st-backend -p8080:8080 march1st-backend
@@ -12,3 +12,6 @@ docker run -itd --name march1st-backend -p8080:8080 march1st-backend
 # Get logs
 docker ps -a
 docker logs --follow --tail 100 march1st-backend
+
+# get container ip
+ docker inspect march1st-mysql | grep '"IPAddress"' | head -n 1
